c8443e17e90f5819775d1eec9534080ceb98ef3d
Sandro <sandroqarqusashvili@gmail.com>
2025-09-07 23:29:51 -0400

    final changes.

--- gtmoviestore/settings.py
+++ gtmoviestore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-p%c7#afpbf5+)#+n!lrw0=obg%9b+jgo6j*v&5m7g_nue01gsz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -119,7 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
